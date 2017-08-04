b59f7c28b3841f3edc981c0e545cba8344ea0f2c
gaotter <gaotter@gmail.com>
2017-08-04 16:11:08 +0200

    Working on edit logic

--- DynamicCv.Web/dynamic-cv-app/src/app/edit-cv/edit-cv-entry/edit-cv-entry.component.ts
+++ DynamicCv.Web/dynamic-cv-app/src/app/edit-cv/edit-cv-entry/edit-cv-entry.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IEntries } from './../../interfaces/i-entires';
+import { DateTimeService } from './../../services/helpers/date-time.service';
 
 @Component({
   selector: 'app-edit-cv-entry',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-cv-entry.component.css']
 })
 export class EditCvEntryComponent implements OnInit {
+  @Input() entry: IEntries;
+  entryModel: IEntries;
+  constructor(private dateTimeService: DateTimeService) { }
 
-  constructor() { }
+  onFromModelChange(ev: string) {
+    if(!(Date.parse(ev))) return;
+    let ticks = this.dateTimeService.toTicks(ev);
+    this.entry.from = ev;
+    this.entry.fromAsUtcJsTicks = ticks;
+  }
+
+  onToModelChange(ev: string) {
+    if(!(Date.parse(ev))) return;
+    let ticks = this.dateTimeService.toTicks(ev);
+    this.entry.to = ev;
+    this.entry.toAsUtcJSTicks = ticks;
+  }
 
   ngOnInit() {
+    this.entryModel = {
+      areas: null,
+      contentText: "test",
+      from: null,
+      id: 1,
+      title: "test",
+      to: null,
+      projects: null,
+      fromAsUtcJsTicks: null,
+      toAsUtcJSTicks: null
+    }
   }
 
 }
--- DynamicCv.Web/dynamic-cv-app/src/app/services/helpers/date-time.service.ts
+++ DynamicCv.Web/dynamic-cv-app/src/app/services/helpers/date-time.service.ts
@@ -8,5 +8,7 @@ export class DateTimeService {
   constructor() { }
 
   tickToLocalStr = (ticks:number) => moment(ticks).format(getLocalDateString());
+  tickToCommonStr = (ticks:number) => moment(ticks).format("YYYY-MM-DD");
+  toTicks = (date:string) => (new Date(date)).getTime();
 
 }
